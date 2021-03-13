b123748bfc0814c1a530da8291b11415df16871c
Amenly <60410674+Amenly@users.noreply.github.com>
2021-03-13 11:26:01 -0600

    Update cyberdrop.py

--- cyberdrop/cyberdrop.py
+++ cyberdrop/cyberdrop.py
@@ -6,19 +6,22 @@ import re
 import argparse
 import threading
 import time
+import platform
 
 import requests
 from bs4 import BeautifulSoup
+from win32_setctime import setctime
 
-from cyberdrop.constants import HEADERS, CHARACTERS
+from .constants import HEADERS, CHARACTERS
 
 
 class CyberDrop:
-    def __init__(self, args, path=os.getcwd(), length=None, album_dir=None):
+    def __init__(self, args, path=os.getcwd(), length=None, album_dir=None, count=0):
         self.url = args.url
         self.path = path
         self.length = length
         self.album_dir = album_dir
+        self.count = count
 
     def scrape_album(self):
         with requests.Session() as s:
@@ -48,40 +51,61 @@ class CyberDrop:
                 future.result
 
     def download(self, tupl):
-        url, filename, timestamp = tupl[0], tupl[1], tupl[2]
+        url, filename, timestamp = tupl
         with requests.Session() as s:
             r = s.get(url, headers=HEADERS)
-        path = os.path.join(self.album_dir, filename)
+        path = self.check_path_exists(self.album_dir, filename)
         with open(path, 'wb') as f:
             for chunk in r.iter_content(chunk_size=1024):
                 f.write(chunk)
+        self.count += 1
+        if platform.system() == 'Windows':
+            setctime(path, timestamp)
         os.utime(path, (timestamp, timestamp))
 
     @staticmethod
     def clean_text(string):
-        pattern = re.compile(r'[.:/\\]')
+        pattern = re.compile(r'[\\/:*?"<>|]')
         new_string = pattern.sub('_', string)
         return new_string
 
     @staticmethod
-    def spinner(event):
+    def check_path_exists(dir, filename):
+        new_path = os.path.join(dir, filename)
+        name, *extension = filename.partition('.')
+        extension = ''.join(extension)
+        c = 1
+        while True:
+            if os.path.isfile(new_path):
+                new_name = name + (' copy' if c == 1 else f' copy {c}')
+                new_path = os.path.join(dir, new_name + extension)
+            else:
+                break
+            c += 1
+        return new_path
+
+    def spinner(self, event):
+        count = 0
         while True:
             for character in CHARACTERS:
-                if event.is_set():
+                if event.is_set() or count > self.length * 2:
                     return None
-                print(f' {character}', end='\r', flush=True)
-                time.sleep(0.5)
+                print(
+                    f' {character} [{self.count}/{self.length}]', end='\r', flush=True)
+                time.sleep(0.1)
+                count += 1
+
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('url', help='a URL to scrape', type=str)
     args = parser.parse_args()
-    e1 = threading.Event()
-    t1 = threading.Thread(target=CyberDrop.spinner, args=(e1,))
-    t1.start()
     cyberdrop = CyberDrop(args)
     tuples = cyberdrop.scrape_album()
+    e1 = threading.Event()
+    t1 = threading.Thread(target=cyberdrop.spinner, args=(e1,))
+    t1.start()
     cyberdrop.prepare_download(tuples)
     e1.set()
 
